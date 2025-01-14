87032e07a683e00bf540918d86f05485945185ad
NgBaoScript <hbkx22kx3@gmail.com>
2025-01-14 15:56:40 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
-import { TaskDashboardComponent } from './components/task-dashboard/task-dashboard.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskFormComponent,
-    TaskDashboardComponent
   ],
   imports: [
     BrowserModule,
